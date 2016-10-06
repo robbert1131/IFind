eb4085da7da7e866f9c5b16d65a431843221a253
LAPTOP-073QCJ84\gm_ro <gm_robbertvd@hotmail.com>
2016-10-06 14:09:45 +0200

    Advanced

--- IFindApp/app/src/main/java/csi/fhict/org/ifindapp/HomePage.java
+++ IFindApp/app/src/main/java/csi/fhict/org/ifindapp/HomePage.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class HomePage extends AppCompatActivity {
     Button bZoeken;
@@ -26,6 +27,26 @@ public class HomePage extends AppCompatActivity {
                 String zoekterm = searchterm.getText().toString();
                 Intent i = new Intent(getApplicationContext(), ProductList.class);
                 i.putExtra("Title", zoekterm);
+                startActivity(i);
+            }
+        });
+
+        Bspecefiek.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                EditText searchterm = (EditText) findViewById(R.id.etZoek);
+                String zoekterm = searchterm.getText().toString();
+                RadioButton radiodesc = (RadioButton) findViewById(R.id.Rja);
+                boolean Desc = radiodesc.isChecked();
+                Integer afstand = 50;
+                Integer minprijs = 0;
+                Integer maxprijs = 500;
+                Intent i = new Intent(getApplicationContext(), ProductList.class);
+                i.putExtra("Title", zoekterm);
+                i.putExtra("Desc", Desc);
+                i.putExtra("km", afstand);
+                i.putExtra("minprijs", minprijs);
+                i.putExtra("maxprijs", maxprijs);
                 startActivity(i);
             }
         });
