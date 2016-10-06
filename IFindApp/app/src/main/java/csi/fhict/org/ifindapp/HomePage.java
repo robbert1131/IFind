7693e75959ace742e2381552159915a210ac7518
LAPTOP-073QCJ84\gm_ro <gm_robbertvd@hotmail.com>
2016-10-06 15:18:37 +0200

    Changes made to gradle custom slider

--- IFindApp/app/src/main/java/csi/fhict/org/ifindapp/HomePage.java
+++ IFindApp/app/src/main/java/csi/fhict/org/ifindapp/HomePage.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+
+import org.florescu.android.rangeseekbar.RangeSeekBar;
 
 public class HomePage extends AppCompatActivity {
     Button bZoeken;
@@ -16,7 +19,23 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        // Setup the new range seek bar
+        final RangeSeekBar<Integer> rangeSeekBar = new RangeSeekBar<Integer>(this);
+        RangeSeekBar<Integer> RangeSeekBarafstand = new RangeSeekBar<Integer>(this);
+        // Set the range
+        rangeSeekBar.setRangeValues(0, 2000);
+        rangeSeekBar.setSelectedMinValue(0);
+        rangeSeekBar.setSelectedMaxValue(2000);
 
+        RangeSeekBarafstand.setRangeValues(0, 1000);
+        RangeSeekBarafstand.setSelectedMinValue(0);
+        RangeSeekBarafstand.setSelectedMaxValue(1000);
+        // Add to layout
+        LinearLayout layout = (LinearLayout) findViewById(R.id.Rprijs);
+        layout.addView(rangeSeekBar);
+
+        final LinearLayout layout1 = (LinearLayout) findViewById(R.id.RAfstand);
+        layout1.addView(RangeSeekBarafstand);
 
         bZoeken = (Button) findViewById(R.id.bZoeken);
         Bspecefiek = (Button) findViewById(R.id.Bspecefiek);
@@ -38,9 +57,9 @@ public class HomePage extends AppCompatActivity {
                 String zoekterm = searchterm.getText().toString();
                 RadioButton radiodesc = (RadioButton) findViewById(R.id.Rja);
                 boolean Desc = radiodesc.isChecked();
-                Integer afstand = 50;
-                Integer minprijs = 0;
-                Integer maxprijs = 500;
+                Integer afstand = 0;
+                Integer minprijs = rangeSeekBar.getSelectedMinValue();
+                Integer maxprijs = rangeSeekBar.getSelectedMaxValue();
                 Intent i = new Intent(getApplicationContext(), ProductList.class);
                 i.putExtra("Title", zoekterm);
                 i.putExtra("Desc", Desc);
