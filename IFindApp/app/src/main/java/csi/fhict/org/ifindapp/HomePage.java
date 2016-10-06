f2212f9d7d28de20658ec7f434a08bf7512970e6
LAPTOP-073QCJ84\gm_ro <gm_robbertvd@hotmail.com>
2016-10-06 13:45:26 +0200

    Homepage updates

--- IFindApp/app/src/main/java/csi/fhict/org/ifindapp/HomePage.java
+++ IFindApp/app/src/main/java/csi/fhict/org/ifindapp/HomePage.java
@@ -1,13 +1,33 @@
 package csi.fhict.org.ifindapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HomePage extends AppCompatActivity {
-
+    Button bZoeken;
+    Button Bspecefiek;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
+
+        bZoeken = (Button) findViewById(R.id.bZoeken);
+        Bspecefiek = (Button) findViewById(R.id.Bspecefiek);
+        bZoeken.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                EditText searchterm = (EditText) findViewById(R.id.etZoek);
+                String zoekterm = searchterm.getText().toString();
+                Intent i = new Intent(getApplicationContext(), ProductList.class);
+                i.putExtra("Title", zoekterm);
+                startActivity(i);
+            }
+        });
     }
 }
